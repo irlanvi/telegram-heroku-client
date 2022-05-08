4a2609d327d0fd60dd0165e30baff20f0e13c24e
irlanvila <irlanvila@outlook.com>
2022-05-09 06:41:43 +0700

    better welcome message

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import { Telegraf } from "telegraf";
+import { Markup, Telegraf } from "telegraf";
 import "dotenv/config";
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => {
-  ctx.reply("Welcome");
+  ctx.replyWithHTML(
+    `I can help you create and manage Heroku apps 
+
+<strong>Go to heroku and get your heroku api key</strong>`,
+    Markup.inlineKeyboard([
+      Markup.button.url(
+        "Heroku API",
+        "https://dashboard.heroku.com/account/applications/authorizations/new"
+      ),
+    ])
+  );
 });
 bot.launch();
 
